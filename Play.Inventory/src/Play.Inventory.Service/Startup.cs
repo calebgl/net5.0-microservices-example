452ae61756c596d6ed68541e00becb97c3af21e9
calebgl <57515964+calebgl@users.noreply.github.com>
2023-02-09 22:41:23 -0600

    feat: implement circuit breaker pattern

--- Play.Inventory/src/Play.Inventory.Service/Startup.cs
+++ Play.Inventory/src/Play.Inventory.Service/Startup.cs
@@ -46,6 +46,10 @@ namespace Play.Inventory.Service
                         retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
                                         + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000))
                     ))
+                    .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+                        3,
+                        TimeSpan.FromSeconds(15)
+                    ))
                     .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
             services.AddControllers();
